155c2a0834b5c1678cc7cef5d6098f9b5eec06ca
Marvin Deuschle <marvdeus@gmail.com>
2022-07-18 16:55:18 +0200

    changed color palette

--- lib/util/bbbb_colors.dart
+++ lib/util/bbbb_colors.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 
 class BBBBColors {
+  // color palette from https://materialpalettes.com/
   static const darkBlueColor = MaterialColor(0xFF041286, {
-    50: Color.fromRGBO(4, 18, 134, .1),
-    100: Color.fromRGBO(4, 18, 134, .2),
-    200: Color.fromRGBO(4, 18, 134, .3),
-    300: Color.fromRGBO(4, 18, 134, .4),
-    400: Color.fromRGBO(4, 18, 134, .5),
-    500: Color.fromRGBO(4, 18, 134, .6),
-    600: Color.fromRGBO(4, 18, 134, .7),
-    700: Color.fromRGBO(4, 18, 134, .8),
-    800: Color.fromRGBO(4, 18, 134, .9),
-    900: Color.fromRGBO(4, 18, 134, 1),
+    50: Color(0xffeae5f4),
+    100: Color(0xffcabfe4),
+    200: Color(0xffa795d3),
+    300: Color(0xff846bc3),
+    400: Color(0xff6a4cb6),
+    500: Color(0xff4e2eaa),
+    600: Color(0xff452aa4),
+    700: Color(0xff37229c),
+    800: Color(0xff281c94),
+    900: Color(0xff041186)
   });
 }
